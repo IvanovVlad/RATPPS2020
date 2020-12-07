c3c1a60a917e4e395d1a1808fc0625edc62edb21
IvanovVlad <rr.rr0@mail.ru>
2020-12-07 13:04:03 +0400

    lr 2

---
+++ second/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace HTTPClient
+{
+    public class Client
+    {
+        public bool Ping(string link)
+        {
+            var client = new WebClient();
+            try
+            {
+                client.DownloadString(link);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        public string GetInputData(string link)
+        {
+            var client = new WebClient();
+            return client.DownloadString(link);
+        }
+
+        public void WriteAnswer(string link, string answer)
+        {
+            var client = new WebClient();
+            var sendstring = Encoding.UTF8.GetBytes(answer);
+            client.UploadData(link, sendstring);
+        }
+    }
+}
---
+++ second/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace HTTPClient
+{
+    class Program
+    {
+        static void Main(string[] args) { }
+    }
+}
